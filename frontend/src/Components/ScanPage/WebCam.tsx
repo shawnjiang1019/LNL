67f60861e4cdcefb137f562275b4642235c961f2
shawnjiang1019 <64927869+shawnjiang1019@users.noreply.github.com>
2023-10-14 00:25:33 -0400

    Added webcam

--- frontend/src/Components/ScanPage/WebCam.tsx
+++ frontend/src/Components/ScanPage/WebCam.tsx
@@ -1,11 +1,33 @@
 import Webcam from "react-webcam";
+import { useRef, useState, useCallback } from "react"; // import useRef
 
 const WebCam = () => {
-  return (
-    <div className="container">
-      <Webcam height={600} width={600} />
-    </div>
-  );
+    const webcamRef = useRef(null); // create a webcam instance
+    const [imgSrc, setImgSrc] = useState(null); // initialize it
+
+
+    // created capture function
+    const capture = useCallback(() => {
+        
+        const imageSrc = (webcamRef.current as any).getScreenshot();
+        setImgSrc(imageSrc);
+      }, [webcamRef]);
+    
+
+    
+    return (
+        <div className="container">
+            {imgSrc ? (
+                <img src={imgSrc} alt="WebCam" />
+            ) : (
+                <Webcam height={600} width={600} ref={webcamRef} />
+            )}
+            <div className="btn-container">
+                <button onClick={capture}>Capture photo</button>
+            </div>
+        </div>
+  
+    );
 };
 
 export default WebCam;
