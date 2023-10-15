601e4c9f691759c5e3280fdfd4703f2ab6fae701
shawnjiang1019 <64927869+shawnjiang1019@users.noreply.github.com>
2023-10-14 20:02:19 -0400

    Commit

--- frontend/src/Components/HomePage/HomePage.tsx
+++ frontend/src/Components/HomePage/HomePage.tsx
@@ -1,9 +1,27 @@
+import { useNavigate } from "react-router";
+import Button from "../Layout/Button";
+import { Link } from "react-router-dom";
+import { color } from "@mui/system";
+
 interface HomePageProps {}
 
 export default function HomePage({}: HomePageProps) {
   return (
     <div>
       <div>What up guys</div>
+      <body>
+        <Link to="/face">
+          <button type="button">
+                Go to Face
+          </button>
+        </Link>
+        <Link to="/PromptPage">
+          <button type="button">
+                Go to Prompt
+          </button>
+        </Link>
+      </body>
+      
     </div>
   );
 }
--- frontend/src/Components/ScanPage/ScanPage.tsx
+++ frontend/src/Components/ScanPage/ScanPage.tsx
@@ -2,21 +2,26 @@ import React, { useState, useEffect } from "react";
 import WebCam from "./WebCam";
 
 export default function ScanPage() {
-  const [imgSrc, setSrc] = useState("");
-  const [songs, results] = useState("");
-
+  const [imgSrc, setSrc] = useState('');
+  const [songs, results] = useState('');
+  
   useEffect(() => {
-    fetch("http://localhost:3001").then(d => d.json()).then(d => results(d));
-      // .then((data) => data.json())
-      // .then((data) => console.log(data));
+    fetch('/top')
+      .then(data => data.json())
+      .then(data => results(data.songs));
   }, []);
+  
 
+  
   return (
     <div>
       <WebCam setSrc={setSrc} />
       {imgSrc && <div>{imgSrc}</div>}
-      {imgSrc && <img src={imgSrc} />}
+      {imgSrc && <img src={imgSrc}/>}
       <h1>{songs}</h1>
+      
+
     </div>
+    
   );
 }
